b376064bfa9605ff617304168f152acab71c93de
NT <usedforfunny@gmail.com>
2022-04-25 18:43:31 +0200

    Initial Commit

---
+++ Notes WatchKit Extension/NotesApp.swift
@@ -0,0 +1,19 @@
+//
+//  NotesApp.swift
+//  Notes WatchKit Extension
+//
+//  Created by Alexandre Talatinian on 25/04/2022.
+//
+
+import SwiftUI
+
+@main
+struct NotesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            NavigationView {
+                ContentView()
+            }
+        }
+    }
+}
