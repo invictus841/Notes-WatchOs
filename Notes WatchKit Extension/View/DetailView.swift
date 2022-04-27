e00b5960d26bd0513557418dbb2993ba0c7ab1d7
NT <usedforfunny@gmail.com>
2022-04-27 11:03:22 +0200

    Added a DetailView, with a header, content and footer

---
+++ Notes WatchKit Extension/View/DetailView.swift
@@ -0,0 +1,75 @@
+//
+//  DetailView.swift
+//  Notes WatchKit Extension
+//
+//  Created by Alexandre Talatinian on 27/04/2022.
+//
+
+import SwiftUI
+
+struct DetailView: View {
+    // MARK: - PROPERTY
+    
+    let note: Note
+    let count: Int
+    let index: Int
+    
+    // MARK: - BODY
+    
+    
+    var body: some View {
+        VStack(alignment: .center, spacing: 3) {
+            // HEADER
+            HStack {
+                Capsule()
+                    .frame(height: 1)
+                
+                Image(systemName: "note.text")
+                
+                Capsule()
+                    .frame(height: 1)
+            } //: HSTACK
+            .foregroundColor(.accentColor)
+            
+            // CONTENT
+            Spacer()
+            
+            ScrollView(.vertical) {
+                Text(note.text)
+                    .font(.title3)
+                    .fontWeight(.semibold)
+                    .multilineTextAlignment(.center)
+            }
+            
+            Spacer()
+            
+            // FOOTER
+            HStack(alignment: .center) {
+                Image(systemName: "gear")
+                
+                Spacer()
+                
+                Text("\(index + 1) / \(count)")
+                
+                Spacer()
+                
+                Image(systemName: "info.circle")
+                    .imageScale(.large)
+                
+            } //: HSTACK
+            .foregroundColor(.secondary)
+            
+        } //: VSTACK
+        .padding(3)
+    }
+}
+
+    // MARK: - PREVIEW
+
+struct DetailView_Previews: PreviewProvider {
+    static var sampleData: Note = Note(id: UUID(), text: "Hello, World!")
+    
+    static var previews: some View {
+        DetailView(note: sampleData, count: 5, index: 1)
+    }
+}
